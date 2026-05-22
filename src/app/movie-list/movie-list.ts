064be14a2f46f23ed56e87875a982f6e88e98ae0
GabrielFernandezMelendez <gabriel.fernandezdeveloper@gmail.com>
2026-05-22 09:47:02 +0200

    feat: basic routing and static pages

---
+++ src/app/movie-list/movie-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-list',
+  standalone: true,
+  templateUrl: './movie-list.html',
+  styleUrl: './movie-list.css'
+})
+export class MovieList {}
---
+++ src/app/movies.ts
@@ -0,0 +1,38 @@
+import { Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Movie, MovieDetail } from './interfaces/movie.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+  private baseUrl = 'https://api.themoviedb.org/3';
+  private imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
+  private token = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZDJkODVlOTgyMzMxNDFkNGFmNGEyNWVkNjJjNDMxZiIsIm5iZiI6MTc3OTQzNDc3NS40MzI5OTk4LCJzdWIiOiI2YTEwMDUxNzEyN2QxYmJkMjJhMzA5NTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.KKtE1YVh6vVEW8D8gMxKjx9zWbR3PWW1TBngoJdFokc';
+
+  private headers = {
+    Authorization: `Bearer ${this.token}`,
+    'Content-Type': 'application/json'
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getPopularMovies(): Observable<{ results: Movie[] }> {
+    return this.http.get<{ results: Movie[] }>(
+      `${this.baseUrl}/movie/popular?language=es-ES&page=1`,
+      { headers: this.headers }
+    );
+  }
+
+  getMovieById(id: string): Observable<MovieDetail> {
+    return this.http.get<MovieDetail>(
+      `${this.baseUrl}/movie/${id}?language=es-ES`,
+      { headers: this.headers }
+    );
+  }
+
+  getPosterUrl(path: string): string {
+    return path ? `${this.imageBaseUrl}${path}` : 'https://via.placeholder.com/500x750?text=Sin+Poster';
+  }
+}
